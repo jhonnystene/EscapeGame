b5e2d3de2de147e5a3c0a4e95a69e0aaf6448595
ethans0061 <ethans0061@gshare.blackgold.ca>
2020-04-24 00:48:36 -0600

    Added simple player movement

--- src/mygame/Main.java
+++ src/mygame/Main.java
@@ -1,15 +1,29 @@
 package mygame;
 
 import com.jme3.app.SimpleApplication;
-import com.jme3.renderer.RenderManager;
+import com.jme3.collision.CollisionResult;
+import com.jme3.collision.CollisionResults;
+import com.jme3.font.BitmapText;
+import com.jme3.input.KeyInput;
+import com.jme3.input.MouseInput;
+import com.jme3.input.controls.ActionListener;
+import com.jme3.input.controls.AnalogListener;
+import com.jme3.input.controls.KeyTrigger;
+import com.jme3.input.controls.MouseButtonTrigger;
+import com.jme3.material.Material;
+import com.jme3.math.ColorRGBA;
+import com.jme3.math.Ray;
+import com.jme3.math.Vector3f;
+import com.jme3.scene.Geometry;
+import com.jme3.scene.Node;
+import com.jme3.scene.shape.Box;
 import com.jme3.system.AppSettings;
 
 public class Main extends SimpleApplication {
-
+    Node pickables;
+    
     public static void main(String[] args) {
         Main app = new Main();
-        
-        // Config window shown on start up?
         app.showSettings = false;
         
         // Create a new app settings loaded with defaults
@@ -25,25 +39,120 @@ public class Main extends SimpleApplication {
         app.setSettings(appSettings);
         app.start();
     }
-
+        
+    protected Geometry player;
+    
+    // Init game
     @Override
     public void simpleInitApp() {
+        initPickups();
+        initKeybinds();
         
-        // Disable fly cam
-        this.flyCam.setEnabled(false);
+        // Disable debug and fps - Press F5 to re-enable in game if you really want
+        setDisplayStatView(false); 
+        setDisplayFps(false); 
         
-        // Debug info and FPS Shown on start up?
-        this.setDisplayFps(false);
-        this.setDisplayStatView(false);
+        this.flyCam.setMoveSpeed(10); // Raise camera speed to an acceptable level
+        
+        // Creates test player
+        Box b = new Box(1, 1, 1);
+        player = new Geometry("Player", b);
+        Material mat = new Material(assetManager, "Common/MatDefs/Misc/Unshaded.j3md");
+        mat.setColor("Color", ColorRGBA.Red);
+        player.setMaterial(mat);
+        rootNode.attachChild(player);
+        
+        // Create test box my guy
+        Geometry box = new Geometry("Box", new Box(1, 1, 1));
+        Material boxMaterial = new Material(assetManager,"Common/MatDefs/Misc/Unshaded.j3md");
+        boxMaterial.setTexture("ColorMap", assetManager.loadTexture("Textures/Terrain/splat/dirt.jpg"));
+        box.setMaterial(boxMaterial);
+        
+        // Make box respond to raycasting
+        // Do the same thing for other objects to make it so they can be picked up
+        pickables.attachChild(box);
     }
-
-    @Override
-    public void simpleUpdate(float tpf) {
-        //TODO: add update code
+    
+    // Function for initializing pickups and crosshair and stuff
+    // Written by Johnny - Don't touch if you don't understand it
+    private void initPickups() {
+        pickables = new Node(); // Java!
+        rootNode.attachChild(pickables);
+        
+        guiFont = assetManager.loadFont("Interface/Fonts/Default.fnt"); // Load a font
+        
+        // Create a text object that says +
+        BitmapText ch = new BitmapText(guiFont, false);
+        ch.setSize(guiFont.getCharSet().getRenderedSize() * 2);
+        ch.setText("+"); // crosshairs
+        ch.setLocalTranslation( // center
+        settings.getWidth() / 2 - ch.getLineWidth()/2,
+        settings.getHeight() / 2 + ch.getLineHeight()/2, 0);
+        
+        guiNode.attachChild(ch); // Throw it in the center
     }
-
-    @Override
-    public void simpleRender(RenderManager rm) {
-        //TODO: add render code
+    
+    // Function for initializing keybinds
+    private void initKeybinds() {
+        inputManager.addMapping("Action", new MouseButtonTrigger(MouseInput.BUTTON_LEFT));
+        inputManager.addMapping("Forward", new KeyTrigger(KeyInput.KEY_I));
+        inputManager.addMapping("Backward", new KeyTrigger(KeyInput.KEY_K));
+        inputManager.addMapping("Left", new KeyTrigger(KeyInput.KEY_J));
+        inputManager.addMapping("Right", new KeyTrigger(KeyInput.KEY_L));
+        inputManager.addMapping("Sprint", new KeyTrigger(KeyInput.KEY_SPACE));
+        inputManager.addListener(actionListener, "Action");
+        inputManager.addListener(analogListener, "Forward", "Backward");
+        inputManager.addListener(analogListener, "Left", "Right");
+        inputManager.addListener(analogListener, "Sprint");
     }
+    
+    // Action input
+    private final ActionListener actionListener = new ActionListener() {
+        @Override
+        public void onAction(String name, boolean keyPressed, float tpf) {
+            if(name.equals("Action") && !keyPressed) { // LMB
+                // Written by Johnny - Don't touch if you don't understand it
+                // Oh boy, do I hate raycasting!
+                CollisionResults results = new CollisionResults();
+                Ray ray = new Ray(cam.getLocation(), cam.getDirection());
+                pickables.collideWith(ray, results); // Actually cast the ray
+                for(int i = 0; i < results.size(); i ++) { // Loop over all items the ray hit
+                    float dist = results.getCollision(i).getDistance();
+                    Vector3f pt = results.getCollision(i).getContactPoint();
+                    String hit = results.getCollision(i).getGeometry().getName();
+                    System.out.println("DEBUG: Raycast hit " + hit + " at " + pt + " " + dist + " wu away.");
+                }
+                
+                if(results.size() > 0) {
+                    CollisionResult closest = results.getClosestCollision();
+                    // TODO: Make item picked up follow camera, Portal-style
+                }
+            }
+        }
+    };
+    
+    // Movement input
+    private final AnalogListener analogListener = new AnalogListener() {
+        @Override
+        public void onAnalog(String name, float value, float tpf) {
+            // Written by Ethan S
+            int playerSpeed = 10;
+            if (name.equals("Right")) {
+                Vector3f v = player.getLocalTranslation();
+                player.setLocalTranslation(v.x + value * playerSpeed, v.y, v.z);
+            }
+            if (name.equals("Left")) {
+                Vector3f v = player.getLocalTranslation();
+                player.setLocalTranslation(v.x - value * playerSpeed, v.y, v.z);
+            }
+            if (name.equals("Forward")) {
+                Vector3f v = player.getLocalTranslation();
+                player.setLocalTranslation(v.x, v.y, v.z - value * playerSpeed);
+            }
+            if (name.equals("Backward")) {
+                Vector3f v = player.getLocalTranslation();
+                player.setLocalTranslation(v.x, v.y, v.z  + value * playerSpeed);
+            }
+        }
+    };    
 }
