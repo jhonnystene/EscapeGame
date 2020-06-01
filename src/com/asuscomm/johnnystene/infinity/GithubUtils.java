a9babf01830ac861f5d6a15414017affb15ed75c
Johnny Stene <jhonnystene@gmail.com>
2020-06-01 09:26:48 -0600

    Delete GithubUtils.java

--- src/com/asuscomm/johnnystene/infinity/GithubUtils.java
+++
@@ -1,7 +0,0 @@
-package com.asuscomm.johnnystene.infinity;
-
-public class GithubUtils {
-	public static String getFullPath(String path) {
-		return "https://github.com/jhonnystene/EscapeGame/raw/master/res/" + path;
-	}
-}
